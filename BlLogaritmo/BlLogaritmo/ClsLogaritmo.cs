f30c734c1df2923c0918fef4f5ec16ca1fc331fa
angiemorenob <126583457+angiemorenob@users.noreply.github.com>
2023-03-28 17:24:58 -0500

    Add files via upload

---
+++ BlLogaritmo/BlLogaritmo/ClsLogaritmo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// método logaritmo
+/// </summary>
+/// <param name="num">operando 1</param>
+/// <param name="log">operando 2</param>
+/// <returns></returns>
+
+namespace BlLogaritmo
+{
+    public class ClsLogaritmo
+    {
+       
+        public static double Logaritmo(double num, double log)
+
+        {
+            return log = Math.Log(num) / Math.Log(log);
+        }
+    }
+}
