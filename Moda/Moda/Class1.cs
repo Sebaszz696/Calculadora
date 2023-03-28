21b36b029f96984ad791444765e1a195c7b81c31
mateobp04 <126584132+mateobp04@users.noreply.github.com>
2023-03-28 11:35:48 -0500

    Add files via upload

---
+++ Moda/Moda/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moda
+{
+    public class ClsModa
+    {
+        public static long Moda(List<long> numeros)
+        {
+            Dictionary<long, int> frecuencias = new Dictionary<long, int>();
+            foreach (long numero in numeros)
+            {
+                if (frecuencias.ContainsKey(numero))
+                    frecuencias[numero]++;
+                else
+                    frecuencias[numero] = 1;
+            }
+            int maxFrecuencia = frecuencias.Values.Max();
+            long moda = frecuencias.FirstOrDefault(x => x.Value == maxFrecuencia).Key;
+            return moda;
+        }
+    }
+}
