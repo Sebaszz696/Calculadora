2f73816ce105bc7fe4d603b46f1a36f248ecf212
Jaider-Oquendo <126584216+Jaider-Oquendo@users.noreply.github.com>
2023-03-28 11:43:25 -0500

    Add files via upload

---
+++ BlExponencial/BlExponencial/ClsExponencial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlExponencial
+{
+    public class ClsExponencial
+    {
+        /// <summary>
+        /// Metodo Exponencial 
+        /// </summary>
+        /// <param name="Num1"></param>
+        /// <param name="Num2"></param>
+        /// <returns></returns>
+        public static long Exponencial(long Num1, long Num2)
+        {
+            return (Num1 ^ Num2);
+        }
+    }
+}
