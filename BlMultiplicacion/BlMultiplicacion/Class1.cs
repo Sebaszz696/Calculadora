ada15afa24aea9f172d22a5653d660447fe1c332
LauraSerenaRM <126583880+LauraSerenaRM@users.noreply.github.com>
2023-03-28 11:26:37 -0500

    Add files via upload
    Holi

---
+++ BlMultiplicacion/BlMultiplicacion/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlMultiplicacion
+{
+    public class ClsMultiplicacion
+    {
+        public static long Multiplicacion(long Numero1, long Numero2, long Numero3, long Numero4)
+        {
+            return (Numero1 * Numero2 * Numero3 * Numero4);
+
+        }
+
+    }
+}
