3e09fba0f4aacdd73d34637f17d34559b26235ba
Meylour <126583911+Meylour@users.noreply.github.com>
2023-03-28 11:42:44 -0500

    Add files via upload

---
+++ BlSeno/Seno/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seno
+{
+    public class ClsSeno
+    {
+        public double Seno(double angulo)
+        {
+            return Math.Sin(angulo);
+        }
+    }
+
+    
+}
