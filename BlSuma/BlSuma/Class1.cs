94221c4da58f23c6588494acd6f48e6af44b2939
JUANESTEBANORTIZRENDON <126583684+JUANESTEBANORTIZRENDON@users.noreply.github.com>
2023-03-28 11:30:58 -0500

    Add files via upload
    Suma

---
+++ BlSuma/BlSuma/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlSuma
+{
+    public class Class1
+    {
+        public static long Suma(long I, long J, long M, long N)
+        {
+            return I+  J + M + N;   
+        }
+
+    }
+}
