93cb2280fbabc7bcc6c4546f1b1665f165b310be
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 17:16:57 -0500

    Add files via upload

---
+++ BlCoseno/BlCoseno/ClsCoseno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ClsCoseno
+    {
+        public static double Coseno(double a, double b, double c, double anguloCoseno, double x=2)
+        {
+            
+            return anguloCoseno = (Math.Pow(a, 2) + Math.Pow(b, 2)) - Math.Pow(c, 2) * (2 * a * b);
+        }
+    }
+}
